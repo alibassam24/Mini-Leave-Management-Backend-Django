22e36d50926d5a3ed33710a54c0f653f55f8ad35
Ali Bassam <alibassam063@gmail.com>
2025-08-19 10:28:03 +0500

    models

--- MiniLeaveBackend/core/models.py
+++ MiniLeaveBackend/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+
+class User(AbstractUser):
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    ROLE_CHOICES=[("e","employee"),("h","hr")]
+    role=models.CharField(max_length=10,choices=ROLE_CHOICES)
+    #id,username,email,password,first name,last name default
+
+    def __str__(self):
+        return self.username
+    
+class EmployeeProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    department=models.CharField(max_length=30)
+    joining_date=models.DateField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    leave_balance=models.PositiveIntegerField(default=5)
+ 
+class HrProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+ 
+class Leave(models.Model):
+
+    pass
+
+class Application(models.Model):
+    pass
