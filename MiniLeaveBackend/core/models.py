5d0bf0ab49fd332a626c2f55724d0db80a1fa432
Ali Bassam <alibassam063@gmail.com>
2025-08-19 12:54:36 +0500

    19-aug-25

--- MiniLeaveBackend/core/models.py
+++ MiniLeaveBackend/core/models.py
@@ -1,60 +1,70 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 # Create your models here.
 
 
-
 class User(AbstractUser):
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    ROLE_CHOICES=[("e","employee"),("h","hr")]
-    role=models.CharField(max_length=10,choices=ROLE_CHOICES)
-    email=models.EmailField(unique=True)
-    #id,username,password,first name,last name default
-    USERNAME_FIELD="email"
-    REQUIRED_FIELDS=["username"]
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    ROLE_CHOICES = [("employee", "employee"), ("hr", "hr")]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    email = models.EmailField(unique=True)
+    # id,username,password,first name,last name default
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     def __str__(self):
         return self.username
-    
+
 
 class EmployeeProfile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    phone_number=models.CharField(max_length=12)
-    department=models.CharField(max_length=30)
-    joining_date=models.DateField()
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    leave_balance=models.PositiveIntegerField(default=5)
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    department = models.CharField(max_length=30)
+    joining_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    leave_balance = models.PositiveIntegerField(default=5)
+
     def __str__(self):
         return self.user.username
 
+
 class HrProfile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    join_date = models.DateField(blank=True, null=True)
+
     def __str__(self):
         return self.user.username
+
 
 class Application(models.Model):
     class StatusChoices(models.TextChoices):
-        PENDING="pending","pending"
-        APPROVED="approved","approved"
-        REJECTED="rejected","rejected"
+        PENDING = "pending", "pending"
+        APPROVED = "approved", "approved"
+        REJECTED = "rejected", "rejected"
+
     class LeaveType(models.TextChoices):
-        SICK="sick","sick"
-        EMERGENCY="emergency","emergency"
-        ANNUAL="annual","annual"
-    employee=models.ForeignKey(EmployeeProfile,on_delete=models.CASCADE)
-    status=models.CharField(max_length=18, default="pending") #pending, accepted rejected
-    leave_type=models.CharField(choices=LeaveType,blank=True,null=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    start_date=models.DateField()
-    end_date=models.DateField()
-    reason_description=models.TextField()
-    rejection_reason=models.TextField(blank=True,null=True)
-    
+        SICK = "sick", "sick"
+        EMERGENCY = "emergency", "emergency"
+        ANNUAL = "annual", "annual"
+
+    employee = models.ForeignKey(EmployeeProfile, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=18, choices=StatusChoices.choices, default=StatusChoices.PENDING
+    )  # pending, accepted rejected
+    leave_type = models.CharField(
+        choices=LeaveType.choices, blank=True, null=True, default=LeaveType.ANNUAL
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    reason_description = models.TextField()
+    rejection_reason = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return self.employee.user.username
--- MiniLeaveBackend/core/views.py
+++ MiniLeaveBackend/core/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework.decorators import (api_view, authentication_classes,
+                                       permission_classes)
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
+
+
+@permission_classes([IsAdminUser])
+@api_view(["POST"])
+def add_hr(request):
+    email = request.GET.get("email")
+    password = request.GET.get("password")
+    role = request.GET.get("role")
+
+@api_view(["POST"])    
+def login(request):
+    pass
+
+@api_view(["POST"])
+def logout(request):
+    pass
+
+@api_view(["POST"])
+def add_employee(request):
+    pass
+
+@api_view(["DELETE"])
+def delete_employee(request):
+    pass
+
+@api_view(["POST"])
+def apply_for_leave(request):
+    pass
+
+
+def approve_leave(request):
+    pass
+
+def reject_leave(request):
+    pass
+
+def get_leave_balance(request,employee_id):
+    pass
