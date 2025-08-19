76195e458d4364a7fe7bf7e05a92f1e6ae8bcd4a
Ali Bassam <alibassam063@gmail.com>
2025-08-19 11:31:30 +0500

    models fixed

--- MiniLeaveBackend/core/models.py
+++ MiniLeaveBackend/core/models.py
@@ -9,11 +9,14 @@ class User(AbstractUser):
     updated_at=models.DateTimeField(auto_now=True)
     ROLE_CHOICES=[("e","employee"),("h","hr")]
     role=models.CharField(max_length=10,choices=ROLE_CHOICES)
-    #id,username,email,password,first name,last name default
-
+    email=models.EmailField(unique=True)
+    #id,username,password,first name,last name default
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["username"]
     def __str__(self):
         return self.username
     
+
 class EmployeeProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     phone_number=models.CharField(max_length=12)
@@ -22,10 +25,10 @@ class EmployeeProfile(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     leave_balance=models.PositiveIntegerField(default=5)
-
+    
     def __str__(self):
         return self.user.username
- 
+
 class HrProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
@@ -34,17 +37,24 @@ class HrProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-# class Leave(models.Model):
-#     pass
-
 class Application(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING="pending","pending"
+        APPROVED="approved","approved"
+        REJECTED="rejected","rejected"
+    class LeaveType(models.TextChoices):
+        SICK="sick","sick"
+        EMERGENCY="emergency","emergency"
+        ANNUAL="annual","annual"
     employee=models.ForeignKey(EmployeeProfile,on_delete=models.CASCADE)
-    status=models.CharField(max_length=18) #pending, accepted rejected
+    status=models.CharField(max_length=18, default="pending") #pending, accepted rejected
+    leave_type=models.CharField(choices=LeaveType,blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     start_date=models.DateField()
     end_date=models.DateField()
-    reason_description=models.CharField(max_length=300)
-    is_approved=models.BooleanField(default=False)
+    reason_description=models.TextField()
+    rejection_reason=models.TextField(blank=True,null=True)
+    
     def __str__(self):
         return self.employee.user.username
